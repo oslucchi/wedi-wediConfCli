06cdfe7421eeeda10f6d331377ba9962dde17074
Stefan Amarie <amarie.stefan@gmail.com>
2020-11-19 18:28:20 +0000

    added baseUrl

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  host: "http://localhost:8080",
+  host: "http://it-configurator.wedi.eu/wediConfTest/restcall",
   baseURL: "/wediConf",
   baseHref: "/"
 };
